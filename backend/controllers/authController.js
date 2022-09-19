e9bc99f00386bb9629e32fac461eaf75cba91a20
Artur Aghasafyants <aghartur@gmail.com>
2022-09-19 14:31:34 +0400

    User Routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,7 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
+const { send } = require('process');
 
 // ! Register a user => /api/v1/register
 
@@ -122,6 +123,53 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   sendToken(user, 200, res);
 });
 
+// * Get currently logged in user details  => /api/v1/me
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// * Update/Change password => /api/v1/password/update
+
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // * Check previous user password
+  const isMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isMatched) {
+    return next(new ErrorHandler('Old password is incorrect'));
+  }
+
+  user.password = req.body.password;
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
+// * Update user profile => /api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  // TODO Update avatar
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
 // * Logout user => /api/v1/logout
 
 exports.logout = catchAsyncErrors(async (req, res, next) => {
@@ -135,3 +183,72 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
     message: 'Logged out',
   });
 });
+
+
+// ! Admin Routes
+
+//* Get All users => api/v1/admin/users
+
+exports.allUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users
+  })
+ })
+
+
+ // !* Get User Details => api/v1/admin/user/:id
+
+ exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if(!user) {
+    return next(new ErrorHandler(`User does not found with id : ${req.params.id}`))
+  }
+
+  res.status(200).json({
+    success: true,
+    user
+  })
+ })
+
+ // * Update user profile ADMIN => /api/v1/admin/user/:id
+
+exports.updateUser = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role
+  };
+
+  
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+ // !* Delete User  => api/v1/admin/user/:id
+
+ exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if(!user) {
+    return next(new ErrorHandler(`User does not found with id : ${req.params.id}`))
+  }
+
+    // TODO - Remove avatar from cloudinary 
+  await user.deleteOne();
+
+  res.status(200).json({
+    success: true,
+  })
+ })
